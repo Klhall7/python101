d48f5e1dc2376fc76f8b9f507d9483d3cf492e78
Klhall7 <156003045+Klhall7@users.noreply.github.com>
2024-01-24 13:23:43 -0500

    Delete week_2 directory
    file replaced with week-2

--- week_2/madlib_funct.py
+++
@@ -1,9 +0,0 @@
-#Write a function that accepts two arguments: a name and a subject.
-# return a string with name & subject interpolated in
-#provide default arguments in case one or both are omitted
-
-def madlib (name = "" , subject = ""): #accepts arguments
-    return f"{name}'s favorite subject is {subject}." #f-string interpolated
- 
-print (madlib ("Jane", "Science")) 
-#call and print function with default arguments
